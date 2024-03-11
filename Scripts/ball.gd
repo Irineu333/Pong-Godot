ca992b42d4de3af729cfe899c8f0e27d7b3314d9
Irineu333 <mmmirineusilva@gmail.com>
2024-03-10 22:27:07 -0300

    Add walls

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
-var direction = Vector2(200, 0)
+var direction = Vector2(200, 200)
 
 func _process(delta):
 	position += direction * delta
 	
 func horizontal():
 	direction.x *= -1
+
+func vertical():
+	direction.y *= -1
