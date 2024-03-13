e224742231c68e76890036f25f4652763e1ab473
Irineu333 <mmmirineusilva@gmail.com>
2024-03-12 23:31:23 -0300

    Configurable action keys

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,16 @@
 extends Area2D
 
+@export var up : Key
+@export var down : Key
+
+var velocity = 500
+
+func _process(delta):
+	if Input.is_key_pressed(up):
+		position.y -= velocity * delta
+	if Input.is_key_pressed(down):
+		position.y += velocity * delta
+
 func _on_area_entered(area):
 	if area.name == "Ball":
 		area.horizontal()
