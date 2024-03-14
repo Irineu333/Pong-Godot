43fc43c157fa9f2d91ac1d2e8b2d645de7539b92
Irineu333 <mmmirineusilva@gmail.com>
2024-03-14 01:05:28 -0300

    Improve interactions

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -2,9 +2,26 @@ extends Area2D
 
 const velocity = 400
 
-var direction = Vector2(1, 1)
+var direction : Vector2
 
+@export_group("Limits")
+@export var top_limit = 0
+@export var bottom_limit = 0
+
+func _ready():
+	var x = [1, -1].pick_random()
+	var y = [1, -1].pick_random()
+	
+	direction = Vector2(x, y)
+	
 func _process(delta):
+	
+	if position.y >= top_limit:
+		vertical()
+		
+	if position.y <= bottom_limit:
+		vertical()
+	
 	position += direction.normalized() * velocity * delta
 	
 func horizontal():
@@ -12,3 +29,6 @@ func horizontal():
 
 func vertical():
 	direction.y *= -1
+
+func _on_body_entered(body):
+	horizontal()
